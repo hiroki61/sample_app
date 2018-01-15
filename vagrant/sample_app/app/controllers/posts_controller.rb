b546072a7f3127e947960e37963cb40e276ff777
北川弘貴 <hiroki.k@kitagawahiroshitakanoMacBook-puro.local>
2018-01-15 19:34:16 +0900

    Finish3-16

--- vagrant/sample_app/app/controllers/posts_controller.rb
+++ vagrant/sample_app/app/controllers/posts_controller.rb
@@ -37,6 +37,6 @@ class PostsController < ApplicationController
   private
 
 	def post_params
-        params.require(:post).permit(:title, :body, :category)
+        params.require(:post).permit(:title, :body, :image)
     end
 end
--- vagrant/sample_app/db/schema.rb
+++ vagrant/sample_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180114135040) do
+ActiveRecord::Schema.define(version: 20180115101636) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20180114135040) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.string "image_id"
   end
 
 end
